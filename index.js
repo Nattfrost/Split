e9c43374ede9ef66692d1878a4feec1d1fda981f
Nils <nilsnathorstwindahl@gmail.com>
2018-12-10 18:14:37 +0100

    Started Experimenting with Colours

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
-
-
 const boxes = [...document.querySelectorAll('.content-section')];
 
-
-
 var options = {
-  threshold: 0.9
+  threshold: 0.6
 }
-
-
 observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     if (entry.intersectionRatio > 0) {
       const currentBoxId = entry.target.dataset.box;
-        entry.target.classList.toggle('visible')
-
+      entry.target.classList.add('visible')
     }
   });
 }, options);
